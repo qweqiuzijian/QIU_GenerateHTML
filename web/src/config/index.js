174eec86557395c573b2cc9d777169bbcd06cde5
求生 <525219691@qq.com>
2018-10-10 15:31:29 +0800

    update

--- web/src/config/index.js
+++ web/src/config/index.js
@@ -1,5 +1,5 @@
 let Config = require('../../config/index')
-const proxyPath = process.env.process.env.NODE_ENV === 'production' ? `${Config.baseUrl_production}/api` : '/api'
+const proxyPath = process.env.NODE_ENV === 'production' ? `${Config.baseUrl_production}/api` : '/api'
 
 export default {
   proxyPath
